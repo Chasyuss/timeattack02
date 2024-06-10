1a22b9d9ec7d94fb5eca3ffdf1dd605b2f9d26bc
chasyuss <cotn963@gmail.com>
2024-06-10 16:37:15 +0900

    todolist 완료

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,1 +1,10 @@
 // TODO: store 셋업하고 slice에서 import한 reducer를 할당하세요.
+
+import { configureStore } from "@reduxjs/toolkit";
+import todosReduceer from "../slices/todoSlice";
+
+export const configStore = configureStore({
+  reducer: {
+    todos: todosReduceer,
+  },
+});
--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -1,1 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
+
 // TODO: todoSlice 를 작성하세요.
+const initialState = {
+  todos: [],
+};
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({
+        id: uuidv4(),
+        title: action.payload.title,
+        content: action.payload.content,
+        isDone: false,
+      });
+    },
+
+    toggleTodo: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.isDone = !todo.isDone;
+      }
+    },
+
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
